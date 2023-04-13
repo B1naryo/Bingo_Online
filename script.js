51f4ae9dde00dff65436f82cdc230b0396d33aeb
sandro520099 <sandro520099@hotmail.com>
2023-04-13 09:48:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sortear() {
-var pessoas = document.getElementById("pessoas").value;
-var lista = pessoas.split('\n');
-var indice = Math.floor(Math.random() * lista.length);
-document.getElementById("resultado").innerHTML = "A pessoa sorteada é: " + lista[indice];
-document.getElementById("resultado").style.display = "block";
+  var pessoas = document.getElementById("pessoas").value;
+  var lista = pessoas.split('\n');
+  var indice = Math.floor(Math.random() * lista.length);
+  document.getElementById("resultado").innerHTML = "A pessoa sorteada é: " + lista[indice];
+  document.getElementById("resultado").style.display = "block";
 }
