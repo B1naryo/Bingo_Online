cc55aa9e9a796c6116eea0b825ffc1597555dcd9
sandro520099 <sandro520099@hotmail.com>
2023-04-13 10:42:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function sortear() {
   var pessoas = document.getElementById("pessoas").value;
   var lista = pessoas.split('\n');
-  var gif = "bexigas2.gif";
   var indice = Math.floor(Math.random() * lista.length);
+  confetti();
   document.getElementById("resultado").innerHTML = "A pessoa sorteada é: " + lista[indice];
   document.getElementById("resultado").style.display = "block";
 }
