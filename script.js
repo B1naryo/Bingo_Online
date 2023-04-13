53fd309e50763224418af532291670a880986ff0
sandro520099 <sandro520099@hotmail.com>
2023-04-13 10:53:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function sortear() {
   var pessoas = document.getElementById("pessoas").value;
   var lista = pessoas.split('\n');
   var indice = Math.floor(Math.random() * lista.length);
-  confetti(); // Chama a função confetti
   document.getElementById("resultado").innerHTML = "A pessoa sorteada é: " + lista[indice];
   document.getElementById("resultado").style.display = "block";
 }
